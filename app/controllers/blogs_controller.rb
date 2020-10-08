2b76cf963e787bd90559344287910f18f0018094
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-08 19:36:46 +0900

    ブログ index画面作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,8 +8,13 @@ class BlogsController < ApplicationController
   end
 
   def create
+    @blog = Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    redirect_to blogs_path
   end
 
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 
-  
 end
