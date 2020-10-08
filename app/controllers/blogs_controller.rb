1ff2ac1f22c9ff0321284d9418a4d1b97e7d6a65
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-08 20:00:19 +0900

    ブログ一覧 削除機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,13 +8,20 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to blogs_path
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
     redirect_to blogs_path
   end
 
   private
   def blog_params
-    params.require(:blog).permit(:title, :content)
+    params.require(:blog).permit(:title, :content, :image)
   end
 
 end
