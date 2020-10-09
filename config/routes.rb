b4f5d79a0dfbd9b256f0ef07482f33b3011c96d9
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-09 17:07:29 +0900

    ログイン画面追加、修正 ユーザー登録機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :blogs do
     collection do
       post :confirm
     end
   end
-  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
 end
