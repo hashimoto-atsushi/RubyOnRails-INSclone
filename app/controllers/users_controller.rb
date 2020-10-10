9faf8bd1bf8a16e8aea28782960c9d5f40912816
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-10 14:34:40 +0900

    ユーザー登録削除機能テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,14 +22,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
     redirect_to new_session_path
+    else
+    render :edit
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
     if current_user.present?
-      debugger
     @blog = Blog.where(user_id: current_user.id)
     @blog.destroy_all
     end
