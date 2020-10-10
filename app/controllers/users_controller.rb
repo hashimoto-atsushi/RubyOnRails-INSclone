0616e0628c1e3e2f9fdc6c605e35a2fcaaef5864
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-10 13:39:39 +0900

    branch arrangeでユーザー削除のテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to new_session_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if current_user.present?
+      debugger
+    @blog = Blog.where(user_id: current_user.id)
+    @blog.destroy_all
+    end
+    @user.destroy
+    redirect_to new_session_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
