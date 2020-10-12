09de1f02000a77d632171de515897fabf9397ccc
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-10-12 16:19:53 +0900

    各コード記述を整理

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,8 @@
 class Blog < ApplicationRecord
-  validates :title, presence: true,
-                      length: { maximum: 20 }
+  validates :title,   presence: true,
+                        length: { maximum: 20 }
   validates :content, presence: true,
-                      length: { maximum: 140 }
+                        length: { maximum: 140 }
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true,
-                     length: { maximum: 20 }
+  validates :name,  presence: true,
+                      length: { maximum: 20 }
   validates :email, presence: true,
-                     length: { maximum: 255},
-                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                      length: { maximum: 255},
+                      format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
